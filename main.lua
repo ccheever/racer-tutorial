6be6291c9294ecb637d4e616cc3bdcad08e1e662
Charlie Cheever <ccheever@gmail.com>
2019-10-26 04:00:44 -0700

    Draw racetrack and draw car

--- main.lua
+++ main.lua
@@ -52,3 +52,29 @@ function createCar()
   }
 end
 
+
+function love.draw()
+  -- Draw the race track 
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.draw(raceTrackImage, 0, 0)
+
+  -- Draw the car
+  local radiansPerSprite = 2 * math.pi / 16
+  local sprite = math.floor((car.rotation + radiansPerSprite / 2) / radiansPerSprite) + 1
+  if car.rotation >= math.pi then
+    sprite = 18 - sprite
+  end
+  drawSprite(carImage, 12, 12, sprite, car.x - 6, car.y - 6, car.rotation >= math.pi)
+end
+
+function drawSprite(spriteSheetImage, spriteWidth, spriteHeight, sprite, x, y, flipHorizontal, flipVertical, rotation)
+  local width, height = spriteSheetImage:getDimensions()
+  local numColumns = math.floor(width / spriteWidth)
+  local col, row = (sprite - 1) % numColumns, math.floor((sprite - 1) / numColumns)
+  love.graphics.draw(spriteSheetImage, 
+    love.graphics.newQuad(spriteWidth * col, spriteHeight * row, spriteWidth, spriteHeight, width, height),
+    x + spriteWidth / 2, y + spriteHeight / 2,
+    rotation or 0,
+    flipHorizontal and -1 or 1, flipVertical and -1 or 1,
+    spriteWidth / 2, spriteHeight / 2)
+end
