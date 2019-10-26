e34ddc8215dea1154c9abe50a66ab35292415a1b
Charlie Cheever <ccheever@gmail.com>
2019-10-26 06:39:43 -0700

    Trimming down to workable version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # racer-tutorial
+
 Racer Tutorial for Castle
+
+https://www.notion.so/castlegames/Racer-Game-bc299213cef74bf588522b98364292c8
--- main.lua
+++ main.lua
@@ -1,44 +1,33 @@
-local log =
-  require "https://raw.githubusercontent.com/ccheever/castle-utils/c5a150bf783bfcaf24bbcf8cbe0824fae34a8198/log.lua"
+local drawSprite =
+  require "https://raw.githubusercontent.com/ccheever/castle-utils/c35e540893e4ee0136d540f3e0ed4f13f840adb2/drawSprite.lua"
+-- local log =
+  -- require "https://raw.githubusercontent.com/ccheever/castle-utils/c5a150bf783bfcaf24bbcf8cbe0824fae34a8198/log.lua"
 
-local GAME_WIDTH = 192
-local GAME_HEIGHT = 192
 local car
-local puffs
-local raceTrackData
-
 local carImage
 local raceTrackImage
-local engineSound
-local crashSound
+local raceTrackData
 
 function love.load()
-  log("loaded")
+  raceTrackImage = love.graphics.newImage("./race-track.png")
+  raceTrackData = love.image.newImageData("./race-track-data.png")
+  carImage = love.graphics.newImage("./car.png")
 
-  -- Load assets
-  carImage = love.graphics.newImage("./img/car.png")
-  raceTrackImage = love.graphics.newImage("./img/race-track.png")
-  --  carImage:setFilter('nearest', 'nearest')
-  -- raceTrackImage:setFilter('nearest', 'nearest')
-  engineSounds = {
-    love.audio.newSource("sfx/engine1.wav", "static"),
-    love.audio.newSource("sfx/engine2.wav", "static"),
-    love.audio.newSource("sfx/engine3.wav", "static"),
-    love.audio.newSource("sfx/engine4.wav", "static")
-  }
-  crashSound = love.audio.newSource("sfx/crash.wav", "static")
-
-  log("Assets loaded:", carImage, raceTrackImage, engineSounds, crashSound)
-
-  -- Load race track data from an image
-  raceTrackData = love.image.newImageData("img/race-track-data.png")
-
-  -- Create the car and an array for the puffs of exhaust
-  puffs = {}
   car = createCar()
 end
 
--- Creates the playable car
+function love.draw()
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.draw(raceTrackImage, 0, 0)
+
+  local radiansPerSprite = 2 * math.pi / 16
+  local sprite = math.floor((car.rotation + radiansPerSprite / 2) / radiansPerSprite) + 1
+  if car.rotation >= math.pi then
+    sprite = 18 - sprite
+  end
+  drawSprite(carImage, 12, 12, sprite, car.x - 6, car.y - 6, car.rotation >= math.pi)
+end
+
 function createCar()
   return {
     x = 95,
@@ -52,46 +41,7 @@ function createCar()
   }
 end
 
-function love.draw()
-  -- Draw the race track
-  love.graphics.setColor(1, 1, 1)
-  love.graphics.draw(raceTrackImage, 0, 0)
-
-  -- Draw the car
-  local radiansPerSprite = 2 * math.pi / 16
-  local sprite = math.floor((car.rotation + radiansPerSprite / 2) / radiansPerSprite) + 1
-  if car.rotation >= math.pi then
-    sprite = 18 - sprite
-  end
-  drawSprite(carImage, 12, 12, sprite, car.x - 6, car.y - 6, car.rotation >= math.pi)
-end
-
-function drawSprite(spriteSheetImage, spriteWidth, spriteHeight, sprite, x, y, flipHorizontal, flipVertical, rotation)
-  local width, height = spriteSheetImage:getDimensions()
-  local numColumns = math.floor(width / spriteWidth)
-  local col, row = (sprite - 1) % numColumns, math.floor((sprite - 1) / numColumns)
-  love.graphics.draw(
-    spriteSheetImage,
-    love.graphics.newQuad(spriteWidth * col, spriteHeight * row, spriteWidth, spriteHeight, width, height),
-    x + spriteWidth / 2,
-    y + spriteHeight / 2,
-    rotation or 0,
-    flipHorizontal and -1 or 1,
-    flipVertical and -1 or 1,
-    spriteWidth / 2,
-    spriteHeight / 2
-  )
-end
-
 function love.update(dt)
-  if love.keyboard.isDown("down") then
-    car.speed = math.max(car.speed - 20 * dt, -10)
-  elseif love.keyboard.isDown("up") then
-    car.speed = math.min(car.speed + 50 * dt, 40)
-  else
-    car.speed = car.speed * 0.98
-  end
-
   -- Turn the car by pressing the left and right keys
   local turnSpeed =
     3 * math.min(math.max(0, math.abs(car.speed) / 20), 1) - (car.speed > 20 and (car.speed - 20) / 20 or 0)
@@ -101,34 +51,48 @@ function love.update(dt)
   if love.keyboard.isDown("right") then
     car.rotation = car.rotation + turnSpeed * dt
   end
+  car.rotation = (car.rotation + 2 * math.pi) % (2 * math.pi)
 
-  -- Move the car
+  if love.keyboard.isDown("down") then
+    -- Press down to brake
+    car.speed = math.max(car.speed - 20 * dt, -10)
+  elseif love.keyboard.isDown("up") then
+    -- Press up to accelerate
+    car.speed = math.min(car.speed + 50 * dt, 40)
+  else
+    -- Slow down when not accelerating
+    car.speed = car.speed * 0.98
+  end
+
+  -- Apply the car's velocity
   car.x = car.x + car.speed * -math.sin(car.rotation) * dt + car.bounceVelocityX * dt
   car.y = car.y + car.speed * math.cos(car.rotation) * dt + car.bounceVelocityY * dt
 
+  -- Check what terrain the car is currently on by looking at the race track data image
   local pixelX = math.min(math.max(0, math.floor(car.x)), 191)
   local pixelY = math.min(math.max(0, math.floor(car.y)), 191)
   local r, g, b = raceTrackData:getPixel(pixelX, pixelY)
-  local isInBarrier = r > 0 -- red = barrier
+  local isInWall = r > 0 -- red = wall
   local isInGrass = b > 0 -- blue = grass
 
+  -- If the car runs off the track, it slows down
   if isInGrass then
     car.speed = car.speed * 0.95
   end
 
-  if isInBarrier then
+  -- If the car becomes lodged in a barrier, bounce it away
+  if isInWall then
     local vx = car.speed * -math.sin(car.rotation) + car.bounceVelocityX
     local vy = car.speed * math.cos(car.rotation) + car.bounceVelocityY
     car.bounceVelocityX = -2 * vx
     car.bounceVelocityY = -2 * vy
     car.speed = car.speed * 0.50
   end
-
-  car.bounceVelocityY = car.bounceVelocityX * 0.90
+  car.bounceVelocityX = car.bounceVelocityX * 0.90
   car.bounceVelocityY = car.bounceVelocityY * 0.90
 
-  -- if the car ever gets out of bounds, reset it
-  if car.x < 0 or car.y < 0 or car.x > 192 or car.y > 192 then
+  -- If the car ever gets out of bounds, reset it
+  if car.x < 0 or car.y < 0 or car.x > 191 or car.y > 191 then
     car = createCar()
   end
 end
